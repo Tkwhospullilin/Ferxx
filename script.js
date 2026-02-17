2fa5be1ad5de4b2b903ec342959f1d1a4ea98d36
TKWhospullilin <torkiabellaha@gmail.com>
2026-02-17 16:03:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const btn = document.getElementById("btnLove");
+const message = document.getElementById("message");
+
+const messages = [
+  "On le savait déjà 😌",
+  "Fernanda = reine officielle du Costa Rica 🇨🇷👑",
+  "Incroyable est un euphémisme.",
+  "Attention, niveau de beauté critique détecté.",
+  "Pura vida mais version ICONIQUE 💃✨"
+];
+
+btn.addEventListener("click", () => {
+  const random = messages[Math.floor(Math.random() * messages.length)];
+  message.textContent = random;
+});
 
